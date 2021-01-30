5ea34ed4ab53ed0360ba02382cc8bdc15b452b8b
davimargelo <davimargelo@hotmail.com>
2021-01-30 12:37:57 -0300

    refactorying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InMemoryDatabase } from './in-memory-database';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    CoreModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,18 +1,15 @@
-import { EntryFormComponent } from './entry-form/entry-form.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { EntriesRoutingModule } from './entries-routing.module';
-import { EntryListComponent } from './entry-list/entry-list.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { IMaskModule } from 'angular-imask';
 import { CalendarModule } from 'primeng/calendar';
-import { IMaskModule } from  'angular-imask';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { EntriesRoutingModule } from './entries-routing.module';
+import { EntryFormComponent } from './entry-form/entry-form.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
 
 @NgModule({
-  declarations: [ EntryListComponent, EntryFormComponent ],
+  declarations: [EntryListComponent, EntryFormComponent],
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
+    SharedModule,
     EntriesRoutingModule,
     CalendarModule,
     IMaskModule
