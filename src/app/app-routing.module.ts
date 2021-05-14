12e58c41bc5d171f73f650f829e8e1d2749211ff
davimargelo <davimargelo@hotmail.com>
2021-05-14 18:38:18 -0300

    redirect to reports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(mod => mod.CategoriesModule) },
   { path: 'entries', loadChildren: () => import('./pages/entries/entries.module').then(mod => mod.EntriesModule) },
-  { path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(mod => mod.ReportsModule) }
+  { path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(mod => mod.ReportsModule) },
+  { path: '', redirectTo: '/reports', pathMatch: 'full' }
 ];
 
 @NgModule({
